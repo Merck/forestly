c8417a33d0b2afb0571369cb83d219550eec5be2
Fukuda, Hiroaki <hiroaki.fukuda@merck.com>
2024-05-19 20:30:27 -0400

    Update to control show and hide SOC column by one button.

--- R/reactable2.R
+++ R/reactable2.R
@@ -111,19 +111,21 @@ reactable2 <- function(
   )
 
   if (soc_toggle) {
-    on_click2 <- paste0("Reactable.setHiddenColumns('", element_id, "', prevColumns => {
-        return prevColumns.length === 0 ? ['soc_name']:[", hidden_item ,"]})")
-    on_click3 <- paste0("Reactable.setHiddenColumns('", element_id, "', prevColumns => {
-        return prevColumns.length === 0 ? [ ]: ['soc_name',",  hidden_item, "]})")
+    on_click2 <- paste0("function control_column(hidden_columns) {",
+                        "  if (hidden_columns.includes('soc_name')) {",
+                        "    Reactable.setHiddenColumns('", element_id, "', prevColumns => {
+                             return prevColumns.length === 0 ? ['soc_name']:[", hidden_item ,"]})",
+                        "  } else {",
+                        "    Reactable.setHiddenColumns('", element_id, "', prevColumns => {
+                             return prevColumns.length === 0 ? [ ]: ['soc_name',",  hidden_item, "]})",
+                        "  }",
+                        "}",
+                        "control_column(Reactable.getState('", element_id, "').hiddenColumns);")
 
     tbl <- htmltools::tagList(
       htmltools::tags$button(
-        "Show SOC column",
+        "Show/Hide SOC column",
         onclick = on_click2
-      ),
-      htmltools::tags$button(
-        "Hide SOC column",
-        onclick = on_click3
       ),
       tbl
     )
