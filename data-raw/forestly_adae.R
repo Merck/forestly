5dc5d0787fb71715deb326639d0d3bc6de2c3dd4
chauhaje <jeetender.chauhan@merck.com>
2025-02-10 11:21:57 -0500

    Hi Ben, Please find 2 programs each for adae and adae_3grp. There is no isssue found in forestly_adae_3grp but forestly_adae dataset has still mismatch for AEACN. I believe as the code try to pick random values from sample, it is assigning these values to any usubjid based on probability mentioned in program. as discussed, I have not saved the dataset, I am only sharing program for your review. Thanks, Jeetender

--- data-raw/forestly_adae.R
+++ data-raw/forestly_adae.R
@@ -1,43 +1,66 @@
 
 library(arsenal)
 library(stringr)
+library(dplyr)
 
 
 
 adae <- r2rtf::r2rtf_adae
 
 
-load("~/forestly/data/forestly_adae_3grp.rda")
-
 
 # Derive AREL from existing AEREL
-adae <- adae %>%
+adae <- adae %>% filter(TRTA!="Xanomeline High Dose") %>%
   mutate(AREL = case_when(
     AEREL %in% c("PROBABLE", "POSSIBLE") ~ "RELATED",
     AEREL %in% c("NONE", "REMOTE", "") ~ "NOT RELATED",
     TRUE ~ AEREL  # Keep original AREL if none of the conditions are met
-  ),
-  TRTA = str_replace_all(as.character(TRTA), "Xanomeline ", "") %>% as.factor()  # Remove "Xanomeline " and convert to factor
-  )
+  ))
+
 
 freq <- adae %>% count(AREL, AEREL) %>%
   arrange(desc(n))
 print(freq)
 
+freq2 <- forestly_adae %>% count(AREL, AEREL) %>%
+  arrange(desc(n))
+print(freq2)
+
 str(adae$TRTA)
-str(forestly_adae_3grp$TRTA)
+str(forestly_adae$TRTA)
 
-# Define the desired order of levels
-desired_levels <- c("Placebo", "Low Dose", "High Dose")
 
-# Standardize TRTA in adae
-adae$TRTA <- factor(adae$TRTA, levels = desired_levels)
 
-source("~/forestly/R/function_dataset0compare.R")
 
-# Call the function to compare the datasets and save the summary
-comparison_result <- compare_datasets(adae, forestly_adae_3grp, "compareresult_adae.html")
+adae$AEACN <- sample(
+  x = c("DOSE NOT CHANGED", "DRUG INTERRUPTED", "DRUG WITHDRAWN", "NOT APPLICABLE", "UNKNOWN"),
+  size = length(adae$USUBJID),
+  prob = c(0.7, 0.1, 0.05, 0.1, 0.05), replace = TRUE
+)
 
-# Save the adae  dataset as an .rda file
-save(adae, file = "~/forestly/data-raw/forestly_adae.rda")
+for (i in seq_along(adae$AEACN)) {
+  adae$action_taken[i] <- switch(adae$AEACN[i],
+                                 "DOSE NOT CHANGED" = "None",
+                                 "DRUG INTERRUPTED" = "Interrupted",
+                                 "DRUG WITHDRAWN" = "Discontinued",
+                                 "NOT APPLICABLE" = "N/A",
+                                 "UNKNOWN" = "Unknown",
+                                 "''" = "None",
+                                 tools::toTitleCase(tolower(adae$AEACN[i]))
+  )
+}
+
+#CHECKS
+# freq <- adae %>% count(AEACN) %>% arrange(desc(n))
+# print(freq)
+#
+#
+# adae %>%
+#   select(USUBJID, TRTAN, TRTA, TRTSDT, ASTDT, ADURN, AETERM, AEREL, AESEQ, TRTEMFL, AREL, AEACN) %>%
+#   slice(1) %>%
+#   print()
+# forestly_adae %>%
+#   select(USUBJID, TRTAN, TRTA, TRTSDT, ASTDT, ADURN, AETERM, AEREL, AESEQ, TRTEMFL, AREL, AEACN) %>%
+#   slice(1) %>%
+#   print()
 
