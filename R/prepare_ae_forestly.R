7551430d4a2806e0e0b463b262699a8734faa48f
Fukuda, Hiroaki <hiroaki.fukuda@merck.com>
2026-04-13 03:41:26 -0400

    Add messages when variables are not available listings

--- R/prepare_ae_forestly.R
+++ R/prepare_ae_forestly.R
@@ -64,6 +64,23 @@ prepare_ae_forestly <- function(
     }
   }
 
+  if (population == observation) {
+    if (any(!meta$population[[population]]$var %in% names(meta$data_observation))) {
+      stop(paste0(
+        "When the same term is specified for population and observation, ",
+        "the variable specified in population should be included in the observation dataset."
+        ))
+    }
+  }
+
+  if (any(!ae_listing_display %in% names(meta$data_observation))) {
+    warning(paste0(
+      "The variables specified in ae_listing_display should be included in the observation dataset. ",
+      "Only the variables included in the observation dataset will be displayed on AE listing table."
+      ))
+    ae_listing_display <- ae_listing_display[ae_listing_display %in% names(meta$data_observation)]
+  }
+
   if (is.null(parameter)) {
     parameters <- names(meta$parameter)
 
